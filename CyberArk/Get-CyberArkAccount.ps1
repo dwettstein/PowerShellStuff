0ba1eb95776feabd0eb6ab08948996f88a3bb536
David Wettstein <dwettstein@gmail.com>
2020-03-13 23:01:01 +0100

    Refactor Get-CyberArkAccount

--- CyberArk/Get-CyberArkAccount.ps1
+++ CyberArk/Get-CyberArkAccount.ps1
@@ -26,7 +26,10 @@
     https://github.com/pspete/psPAS
 
 .EXAMPLE
-    Example of how to use this cmdlet
+    $Account = & ".\Get-CyberArkAccount.ps1" "example.com" "query params"
+
+.EXAMPLE
+    $Account = & "$PSScriptRoot\Get-CyberArkAccount.ps1" -Server "example.com" -Query "query params" -AsJson
 #>
 [CmdletBinding()]
 [OutputType([String])]
@@ -44,10 +47,10 @@ param (
     [Switch] $AsJson
     ,
     [Parameter(Mandatory = $false, Position = 4)]
-    [String] $Username = "${env:USERNAME}"  # secure string or plain text (not recommended)
+    [String] $AuthorizationToken = $null
     ,
     [Parameter(Mandatory = $false, Position = 5)]
-    [String] $Password = $null  # secure string or plain text (not recommended)
+    [Switch] $AcceptAllCertificates = $false
 )
 
 $ErrorActionPreference = "Stop"
@@ -89,32 +92,30 @@ Write-Verbose "$($FILE_NAME): CALL."
 #trap { Write-Error $_; exit 1; break; }
 
 try {
-    $CyberArkToken = & "$FILE_DIR\Connect-CyberArk.ps1" -Server $Server -Username $Username -Password $Password
-
-    $BaseUrl = "https://$Server"
     $EncodedQuery = [System.Web.HttpUtility]::UrlEncode($Query)
-    $EndpointUrl = "$BaseUrl/PasswordVault/api/Accounts?search=$EncodedQuery"
+    $QueryEndpoint = "/PasswordVault/api/Accounts?search=$EncodedQuery"
     if ($Safe) {
         $EncodedSafe = [System.Web.HttpUtility]::UrlEncode("safename eq $Safe")
-        $EndpointUrl += "&filter=$EncodedSafe"
+        $QueryEndpoint += "&filter=$EncodedSafe"
     }
 
-    $Headers = @{
-        "Authorization" = $CyberArkToken
-        "Accept" = "application/json"
-        "Content-Type" = "application/json"
-        "Cache-Control" = "no-cache"
+    if ($AcceptAllCertificates) {
+        $Response = & "$FILE_DIR\Invoke-CyberArkRequest.ps1" -Server $Server -Method "GET" -Endpoint $QueryEndpoint -AuthorizationToken $AuthorizationToken -AcceptAllCertificates
+    } else {
+        $Response = & "$FILE_DIR\Invoke-CyberArkRequest.ps1" -Server $Server -Method "GET" -Endpoint $QueryEndpoint -AuthorizationToken $AuthorizationToken
     }
-
-    $Response = Invoke-WebRequest -Method Get -Headers $Headers -Uri $EndpointUrl
     $ResponseObj = ConvertFrom-Json $Response.Content
     Write-Verbose "Found $($ResponseObj.count) account(s) matching the query '$Query'."
 
     $Accounts = $ResponseObj.value
 
     foreach ($Account in $Accounts) {
-        $EndpointUrl = "$BaseUrl/PasswordVault/api/Accounts/$($Account.id)/Password/Retrieve"
-        $Response = Invoke-WebRequest -Method Post -Headers $Headers -Uri $EndpointUrl
+        $RetrieveEndpoint = "/PasswordVault/api/Accounts/$($Account.id)/Password/Retrieve"
+        if ($AcceptAllCertificates) {
+            $Response = & "$FILE_DIR\Invoke-CyberArkRequest.ps1" -Server $Server -Method "GET" -Endpoint $RetrieveEndpoint -AuthorizationToken $AuthorizationToken -AcceptAllCertificates
+        } else {
+            $Response = & "$FILE_DIR\Invoke-CyberArkRequest.ps1" -Server $Server -Method "GET" -Endpoint $RetrieveEndpoint -AuthorizationToken $AuthorizationToken
+        }
         Add-Member -InputObject $Account -MemberType NoteProperty -Name "secretValue" -Value $Response.Content.Trim('"') -Force
     }
 
@@ -132,10 +133,6 @@ try {
     $ErrorOut = "$($_.Exception.Message)"
     $ExitCode = 1
 } finally {
-    if ($null -ne $CyberArkToken) {
-        $null = Invoke-WebRequest -Method Post -Headers @{"Authorization" = $CyberArkToken } -Uri "$BaseUrl/PasswordVault/api/auth/Logoff"
-    }
-
     $EndDate = [DateTime]::Now
     Write-Verbose ("$($FILE_NAME): ExitCode: {0}. Execution time: {1} ms. Started: {2}." -f $ExitCode, ($EndDate - $StartDate).TotalMilliseconds, $StartDate.ToString('yyyy-MM-dd HH:mm:ss.fffzzz'))
 
